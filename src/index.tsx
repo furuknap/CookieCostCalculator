e94bf8c720fa73636a8b74a4c69480cc76a86abf
Bjørn Furuknap <furuknap@users.noreply.github.com>
2024-12-16 13:56:40 -0500

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import CookieCostCalculator from "./components/CookieCostCalculator";
-import "./index.css"; // If you have global styles
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
